6dbbdc9fdd37a0d2c3a7a9774e00737a2ea00023
Dominik <dominikgos3@gmail.com>
2022-06-07 23:53:13 +0200

    updated authentication views

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 
     <div class="main-section d-flex w-100 justify-content-center p-5 pt-5">
-        <div class="main-section__element d-flex flex-column align-items-center justify-content-center mt-3 w-50">
+        <div class="main-section__element d-flex flex-column align-items-center justify-content-center mt-3 w-25">
             <form class="w-100 mb-5" method="POST" action="{{ route('login') }}">
 
                 @csrf
 
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ old('email') }}">
+                    <input type="email" name="email" class="bg-white form-control @error('email') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ old('email') }}">
 
                     @error('email')
 
@@ -23,7 +23,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="exampleInputPassword1">
+                    <input type="password" name="password" class="bg-white form-control @error('password') is-invalid @enderror" id="exampleInputPassword1">
 
                     @error('password')
                         <div class="invalid-feedback">
--- resources/views/authentication/register.blade.php
+++ resources/views/authentication/register.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 
     <div class="main-section d-flex w-100 justify-content-center p-5 pt-5">
-        <div class="main-section__element d-flex align-items-center justify-content-center mt-3 w-50">
+        <div class="main-section__element d-flex align-items-center justify-content-center mt-3 w-25">
             <form class="w-100" action="{{ route('register') }}" method="POST">
 
                 @csrf
 
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Login</label>
-                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
+                    <input type="text" name="name" class="bg-white form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
 
                     @error('name')
                         <div class="invalid-feedback">
@@ -21,7 +21,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}">
+                    <input type="email" name="email" class="bg-white form-control @error('email') is-invalid @enderror" value="{{ old('email') }}">
 
                     @error('email')
                         <div class="invalid-feedback">
@@ -30,13 +30,12 @@
 
                         @else
                             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-
                     @enderror
 
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
+                    <input type="password" name="password" class="bg-white form-control @error('password') is-invalid @enderror">
 
                     @error('password')
                         <div class="invalid-feedback">
