30e72234377599f3190a6ad4a4288360b66d06c5
Dominik <dominikgos3@gmail.com>
2022-06-10 20:09:17 +0200

    added update profile test methods

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -17,8 +17,7 @@ class UserTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $response = $this->actingAs($user)
-            ->get(route('profile', ['id' => $user->id]));
+        $response = $this->get(route('profile', ['id' => $user->id]));
 
         $response->assertStatus(200)
             ->assertViewIs('profile')
@@ -26,5 +25,56 @@ class UserTest extends TestCase
                 'user',
                 'dataToDisplay',
             ]);
+    }
+
+    public function test_user_can_view_edit_profile_form()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('profile.edit', ['id' => $user->id]));
+
+        $response->assertStatus(200)
+            ->assertViewIs('profile.edit');
+    }
+
+    public function test_user_can_update_own_profile()
+    {
+        $user = User::factory()->create();
+
+        $updatedUser = User::factory()->make(['id' => $user->id]);
+
+        $response = $this->actingAs($user)
+            ->put(
+                route('profile.update', ['id' => $user->id]),
+                [
+                    'name' => $updatedUser->name,
+                    'avatar' => $updatedUser->avatar
+                ]
+            );
+
+        $response->assertRedirect(route('profile', ['id' => $user->id]))
+            ->assertSessionHas('profile-update-success');
+    }
+
+    public function test_user_cannot_update_not_his_profile()
+    {
+        $firstUser = User::factory()->create();
+
+        $secondUser = User::factory()->create();
+
+        $updatedFirstUser = User::factory()->make(['id' => $firstUser->id]);
+
+        $response = $this->actingAs($firstUser)
+            ->put(
+                route('profile.update', ['id' => $firstUser->id]),
+                [
+                    'name' => $updatedFirstUser->name,
+                    'avatar' => $updatedFirstUser->avatar
+                ]
+            );
+
+        $response->assertRedirect(route('profile', ['id' => $firstUser->id]))
+            ->assertStatus(403);
     }
 }
